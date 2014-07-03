dc3af3c8d1c2ce92b4dcdce49fef65d769cb86e6
陆辉 <hui.kang@alipay.com>
2014-07-03 14:26:26 +0800

    update totoro and refactor code style

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-var fs = require('fs')
-var path = require('path')
+var fs = require('fs');
+var path = require('path');
 
-var TotoroClient = require('totoro')
-var TotoroReport = require('./lib/report')
+var TotoroClient = require('totoro');
+var TotoroReport = require('./lib/report');
 
-require('shelljs/global')
+var shelljs = require('shelljs');
 
-var env = process.env
-var tempDir = path.normalize((env.TMPDIR || env.TMP || env.TEMP || '/tmp') + '/totorox')
-var tempTestDir = path.join(tempDir, 'tests')
+var env = process.env;
+var tempDir = path.normalize((env.TMPDIR || env.TMP || env.TEMP || '/tmp') + '/totorox');
+var tempTestDir = path.join(tempDir, 'tests');
 
-mkdir('-p', tempTestDir)
+shelljs.mkdir('-p', tempTestDir);
 
 // console.info('create temp test dir:', tempTestDir)
 exports.run = function(codes, opts) {
-    var scripts = []
+  var scripts = [];
 
-    codes.split(',').forEach(function(code) {
-        if (/\.js$/.test(code)) {
-            scripts.push('<script src="' + code + '"></script>')
-            cp('-Rf', code, tempTestDir)
-        } else {
-            scripts.push('<script>console.log(' + code + ')</script>' )
-        }
-    })
+  codes.split(',').forEach(function(code) {
+    if (/\.js$/.test(code)) {
+      scripts.push('<script src="' + code + '"></script>');
+      shelljs.cp('-Rf', code, tempTestDir);
+    } else {
+      scripts.push('<script>console.log(' + code + ')</script>' );
+    }
+  });
 
-    var runner = fs.readFileSync(path.join(path.dirname(module.filename), 'template/runner.html')) + ''
+  var runner = fs.readFileSync(path.join(path.dirname(module.filename), 'template/runner.html')) + '';
 
-    runner = runner.slice(0, runner.indexOf('<body>') + 6) + scripts.join('') +
-             runner.slice(runner.indexOf('<body>') + 6)
+  runner = runner.slice(0, runner.indexOf('<body>') + 6) + scripts.join('') +
+            runner.slice(runner.indexOf('<body>') + 6);
 
-    fs.writeFileSync(path.join(tempTestDir, 'runner.html'), runner)
+  fs.writeFileSync(path.join(tempTestDir, 'runner.html'), runner);
 
-    runTotoro(opts)
-}
+  runTotoro(opts);
+};
 
 function runTotoro(opts) {
-    if (opts.v || opts.version) {
-        var mainConfig = require('./package.json')
-        var totoroConfig = require('./node_modules/totoro/package.json')
+  if (opts.v || opts.version) {
+    var mainConfig = require('./package.json');
+    var totoroConfig = require('./node_modules/totoro/package.json');
 
-        console.info('totorox vesion: ' + mainConfig.version)
-        console.info('totoro vesion: ' + totoroConfig.version)
-        return
-    }
-    opts.runner = path.join(tempTestDir, 'runner.html')
-    opts.repo = 'totorox'
-    opts.report = function() {
-        TotoroReport.apply(null, arguments)
-    }
-    /**
-    var write = process.stdout.write
-    process.stdout.write = function() {
-        write.apply(process.stdout, [].slice(arguments, 0))
-    }
-    **/
-    new TotoroClient(opts)
+    console.info('totorox vesion: ' + mainConfig.version);
+    console.info('totoro vesion: ' + totoroConfig.version);
+    return;
+  }
+  opts.runner = path.join(tempTestDir, 'runner.html');
+  opts.repo = 'totorox';
+  opts.report = function() {
+    TotoroReport.apply(null, arguments);
+  };
+  /**
+  var write = process.stdout.write
+  process.stdout.write = function() {
+      write.apply(process.stdout, [].slice(arguments, 0))
+  }
+  **/
+  new TotoroClient(opts);
 }
